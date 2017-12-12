603e6032c990a1877578d90fac1b2261f1f0ac79
Johnny Wang <johnnwa@microsoft.com>
2017-12-12 20:57:48 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+
+    public class DateArithmetic
+    {
+        public static void Main()
+        {
+            DateTime date1 = DateTime.Now;
+            Console.WriteLine("{0} ", date1);
+        }
+    }
+}
+
+
+
+
